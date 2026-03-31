89991c8012c219f3647a3cb009fa26c88821df2e
wall-xavier <xvwall@gmail.com>
2026-03-30 18:33:55 -0600

    Corrected directory structure in github_setup.sh

--- Packer/Github-Runner/Scripts/github_setup.sh
+++ Packer/Github-Runner/Scripts/github_setup.sh
@@ -7,8 +7,8 @@ sudo yum -y install packer
 
 # Install Github tools
 cd /usr/src/
-sudo chown -R github:github actions-runner/
 mkdir actions-runner && cd actions-runner
+sudo chown -R github:github actions-runner/
 curl -o actions-runner-linux-x64-2.333.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.333.1/actions-runner-linux-x64-2.333.1.tar.gz
 echo "18f8f68ed1892854ff2ab1bab4fcaa2f5abeedc98093b6cb13638991725cab74  actions-runner-linux-x64-2.333.1.tar.gz" | shasum -a 256 -c
 tar xzf ./actions-runner-linux-x64-2.333.1.tar.gz
